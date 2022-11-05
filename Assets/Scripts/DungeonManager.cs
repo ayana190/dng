432413a909b8e6e743758b457944bb87095c238e
minhtet <107600558+SukegawaShouta@users.noreply.github.com>
2022-11-05 19:09:59 +0900

    aaaa
    aaaaaa

---
+++ Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,369 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    //  マップチップのプレハブ
+    [SerializeField]
+    private GameObject _mapParts = null;
+
+    //  親オブジェクト
+    [SerializeField]
+    private Transform _parent = null;
+
+    //  マップチップスプライト
+    [SerializeField]
+    private List<Sprite> _mapChipSprites = new List<Sprite>();
+
+    private int[,,] _mapDataList = new int[,,]
+    {
+        {
+        //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0x0107,0,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,0,0,0,1,1,1,0,0,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1},
+            {1,0,1,1,2,0,0,0,0,0,0,0,0x0109,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,0x0104,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,0x0107,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,0,0,0,0,0,0x1009,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        },
+        {
+        //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,1,1},
+            {1,0,1,1,1,1,1,1,0,0,1,0,1,1,1,0,0,0,1,1},
+            {1,0,1,1,1,1,0,0,0,1,1,6,1,1,1,0,6,0,1,1},
+            {1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1},
+            {1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,0,1,1,3,0,0,0,0,0,0,0,6,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,1,1,0,1,1,1,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,2,6,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        },
+        {
+        //   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,0,0,0,0,0,0x0107,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,6,1,1,1},
+            {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+            {1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1},
+            {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+            {1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+            {1,1,3,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+        }
+    };
+
+
+    [SerializeField, Header("プレイヤーをアタッチする")]
+    private PlayerView _playerView = null;
+
+    // 生成したマップオブジェクトを格納する場所
+    private List<ChipView> _chipViews = new List<ChipView>();
+
+    //　現在の階数　（0: 1階、1:2階...）
+    private int _mapFloor = 0;
+
+    //  当たり判定のマップキャラクター番号
+    private List<int> _mapHitTable = new List<int>() { 1, 4, 7, 8 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //移動終了のコールバックを登録
+        _playerView.SetupWalkEndCallback(EventWalkEndCallback);
+        MapMake();
+    }
+
+    /// <summary>
+    /// マップデータの取得
+    /// </summary>
+    /// <param name="pos">移動先座標</param>
+    /// <returns>マップデータ</returns>
+    private int GetMapData(Vector3Int pos)
+    {
+        return _mapDataList[_mapFloor, pos.y, pos.x] & 0xff;
+    }
+
+    /// <summary>
+    /// マップステータス
+    /// </summary>
+    /// <param name="pos">移動先座標</param>
+    /// <returns>マップ属性</returns>
+    private int GetMapStat(Vector3Int pos)
+    {
+        return _mapDataList[_mapFloor, pos.y, pos.x] >> 8;
+    }
+    /// <summary>
+    /// 移動可能な場所かどうかのチェック
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    private bool IsWalkEnable(Vector3Int pos)
+    {
+        int mapData = GetMapData(pos);      //  マップデータの取得
+        int mapStat = GetMapStat(pos);      //  マップ情報の取得
+        //  マップが壁であり、通り抜け可能なら true を返す
+        if (1 == mapStat && 1 == mapData)
+            return true;
+        //  当たり判定のチップデータを順番にマップデータと比較してヒットしたら移動不可なので false を返す
+        foreach (int mapHitChip in _mapHitTable)
+        {
+            if (mapHitChip == mapData)
+                return false;
+        }
+        //  どのチップとも一致しなければ通り抜け可能なので true を返す
+        return true;
+    }
+
+    private void MapMake()
+    {
+        //  y を０から１９まで変化させる
+        foreach (int y in Enumerable.Range(0, 20))
+        {
+            //  x を０から１９まで変化させる
+            foreach (int x in Enumerable.Range(0, 20))
+            {
+                //  プレハブの実態をヒエラルキーに生成する
+                GameObject gobj = Instantiate(_mapParts, _parent);
+                //  表示座標を設定する
+                gobj.transform.localPosition = new Vector3(-304 + x * 32, 304 - y * 32, 0);
+                //マップチップデータの取得
+                int mData = GetMapData(new Vector3Int(x, y, 0));
+                //  マップスプライトの設定
+                gobj.GetComponent<ChipView>().SetImage(_mapChipSprites[mData]);
+                //マップオブジェクトの格納
+                _chipViews.Add(gobj.GetComponent<ChipView>());
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// マップの再描画
+    /// </summary>
+    private void RedrawMap()
+    {
+        //  y を０から１９まで変化させる
+        foreach (int y in Enumerable.Range(0, 20))
+        {
+            //  x を０から１９まで変化させる
+            foreach (int x in Enumerable.Range(0, 20))
+            {
+                int index = y * 20 + x;
+                int mData = GetMapData(new Vector3Int(x, y, 0));
+                _chipViews[index].SetImage(_mapChipSprites[mData]);
+
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        //右の矢印を押した？
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Right)))
+                _playerView.WalkAction(PlayerView.PlayerDirection.Right);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Left)))
+                _playerView.WalkAction(PlayerView.PlayerDirection.Left);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Back)))
+                _playerView.WalkAction(PlayerView.PlayerDirection.Back);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (IsWalkEnable(_playerView.GetNextPosition(PlayerView.PlayerDirection.Front)))
+                _playerView.WalkAction(PlayerView.PlayerDirection.Front);
+        }
+        else if (false == _playerView.IsWalking)
+        {
+            _playerView.SetAnimationState(PlayerView.PlayerMode.Idle);
+            //　移動終了してスペースキー押した場合
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // マップのイベントをチェックする
+                MapEventCheck();
+            }
+        }
+    }
+
+    #region イベントチェック
+    private enum AroundDirection
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+
+    /// <summary>
+    /// 移動で終了で呼び出されるコールバック関数
+    /// </summary>
+    private void EventWalkEndCallback()
+    {
+        //　落とし穴チェック 
+        HoleCheck();
+        //ワープチェック
+        WarpCheck();
+    }
+
+    /// <summary>
+    /// 座標から　index を取得する
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    private int GetObjectIndex(Vector3Int pos)
+    {
+        return pos.y * 20 + pos.x;
+    }
+    private Vector3Int GetAroundPos(AroundDirection aroundDirection)
+    {
+        switch (aroundDirection)
+        {
+            case AroundDirection.Left:
+                return _playerView.PlayerPos + Vector3Int.left;
+            case AroundDirection.Right:
+                return _playerView.PlayerPos + Vector3Int.left;
+            case AroundDirection.Up:
+                return _playerView.PlayerPos - Vector3Int.up;
+            case AroundDirection.Down:
+                return _playerView.PlayerPos - Vector3Int.down;
+        }
+        return _playerView.PlayerPos;
+    }
+
+    /// <summary>
+    /// マップのイベントチェック
+    /// </summary>
+    private void MapEventCheck()
+    {
+        if (UpFloorCheck())
+            DownFloorCheck();
+    }
+    /// <summary>
+    /// 登り階段チェック
+    /// </summary>
+    /// <returns>登れたら　folse</returns>
+    private bool UpFloorCheck()
+    {
+        int mdata = GetMapData(_playerView.PlayerPos);
+        if (2 == mdata)
+        {
+            _mapFloor++;
+            RedrawMap();
+            Debug.Log(_mapFloor);//デバックあるか教えてくれるやつ
+            return false;
+            
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 下りチェック
+    /// </summary>
+    private void DownFloorCheck()
+    {
+       int mdata = GetMapData(_playerView.PlayerPos);
+       if (3 == mdata)
+        {
+            _mapFloor--;
+            RedrawMap();
+            Debug.Log(_mapFloor);//デバックあるか教えてくれるやつ
+        }
+    }
+
+    /// <summary>
+    /// 落とし穴チェック
+    /// </summary>
+    private void HoleCheck()
+    {
+        int mdata = GetMapData(_playerView.PlayerPos);
+        if (6 == mdata)
+        {
+            _mapFloor--;
+            RedrawMap();
+            Debug.Log(_mapFloor); //デバックあるか教えてくれるやつ
+        }
+    }
+    
+    /// <summary>
+    /// ワープチェック
+    /// </summary>
+    private void WarpCheck()
+    {
+        // 最初のマップデータとステータスを取得する
+        int mData = GetMapData(_playerView.PlayerPos);
+        int mStat = GetMapStat(_playerView.PlayerPos) >> 4;
+        // ワープポイントかどうかのチェック
+        if (9 == mData)
+        {
+            
+            //  y を０から１９まで変化させる
+            foreach (int y in Enumerable.Range(0, 20))
+            {
+                //  x を０から１９まで変化させる
+                foreach (int x in Enumerable.Range(0, 20))
+                {
+                    Vector3Int pos = new Vector3Int(x, y, 0);
+                    int        md  = GetMapData(pos);
+                    int        sd  = GetMapStat(pos) & 0x0f;
+                    // マップがワープポイントで移動先のインデックスと移動予定のインデックスが一致すればそこからワープ先
+                    if (9 == md && mStat == sd)
+                    {
+                        _playerView.SetPlayerPosition (pos);
+                        return;  
+                    }
+                }
+
+            }
+        }
+            
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/PlayerView.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    #region データ関数
+
+
+    //  アニメーションに関連あるプレイヤーの状態
+    public enum PlayerMode
+    {
+        Idle = 0,
+        RightWalk = 1,
+        FrontWalk = 2,
+        LeftWalk = 3,
+        BackWalk = 4
+    }
+
+    //  プレイヤーの向き
+    public enum PlayerDirection
+    {
+        None = 0,
+        Right = 1,
+        Front = 2,
+        Left = 3,
+        Back = 4
+    }
+    //  プレイヤーの状態
+    private PlayerMode _playerMode = PlayerMode.Idle;
+
+    //  プレイヤーアニメーションのアニメーターを設定
+    [SerializeField]
+    private Animator _animator = null;
+    
+    private Vector3 _ofp = Vector3.zero;
+
+    private Dictionary<PlayerDirection, Vector3> _playerWalkAddLists = new Dictionary<PlayerDirection, Vector3>()
+    {
+        { PlayerDirection.None , Vector3.zero}, 
+        { PlayerDirection.Right, new Vector3(1f, 0f, 0f) },
+        { PlayerDirection.Front, new Vector3(0f, -1f, 0f) },
+        { PlayerDirection.Left, new Vector3(-1f, 0f, 0f) },
+        { PlayerDirection.Back, new Vector3(0f, 1f, 0f) },
+    };
+    private Dictionary<PlayerDirection, Vector3Int> _playerPosAddLists = new Dictionary<PlayerDirection, Vector3Int>()
+    {
+        { PlayerDirection.None , Vector3Int.zero}, 
+        { PlayerDirection.Right, new Vector3Int(1, 0, 0) },
+        { PlayerDirection.Front, new Vector3Int(0, 1, 0) },
+        { PlayerDirection.Left, new Vector3Int(-1, 0, 0) },
+        { PlayerDirection.Back, new Vector3Int(0, -1, 0) },
+    };
+    //  移動速度
+    [SerializeField]
+    private float _walkSpeed = 1f;
+    //  移動中かどうかの判定フラグ
+    private bool _isWalking = false;
+    public  bool IsWalking => _isWalking;
+    
+    //  プレイヤー初期座標
+    public Vector3Int PlayerPos { get; private set; } = new Vector3Int(1, 1, 0);
+
+    private static readonly int     PlayerAnimStat        = Animator.StringToHash("PlayerAnimStat");
+    private static readonly Vector3 PlayerInitialPosition = new Vector3(-272f, 272f, 0f);
+    private static readonly Vector3 MapLeftTopPosition    = new Vector3(-304,  304,  0);
+
+    //  移動終了時呼び出すコールバック関数
+    private Action _walkEndCallback = null;
+
+    #endregion
+
+    private void Awake()
+    {
+        //  プレイヤーの初期座標を指定する
+        transform.localPosition = PlayerInitialPosition;
+    }
+    /// <summary>
+    /// 移動終了時に呼び出す　callback　関数の登録
+    /// </summary>
+    /// <param name="walkEndCallback"></param>
+    public void SetupWalkEndCallback(Action walkEndCallback)
+    {
+        _walkEndCallback = walkEndCallback;
+    }
+
+    /// <summary>
+    /// 外部よりアニメーションのモードを切り替える
+    /// </summary>
+    /// <param name="playerMode">アニメーションの状態</param>
+    public void SetAnimationState(PlayerMode playerMode)
+    {
+        _animator.SetInteger(PlayerAnimStat, (int)playerMode);
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    /// <param name="playerDirection">移動方向</param>
+    public void WalkAction(PlayerDirection playerDirection)
+    {
+        //  すでに移動中ならば何もしない
+        if (_isWalking) return;
+        _isWalking = true;
+        SetAnimationState((PlayerMode)playerDirection);
+        //  移動開始
+        StartCoroutine(Walking(playerDirection));
+    }
+
+    /// <summary>
+    /// プレイヤーの位置を設定する
+    /// </summary>
+    /// <param name="pos"></param>
+    public void SetPlayerPosition(Vector3Int pos)
+    {
+        //プレイヤーの管理座標を更新
+        PlayerPos = pos;
+        //プレイヤーの位置座標を更新
+        transform.position =
+            MapLeftTopPosition + (Vector3)(Vector3Int.right * pos.x  * 32 + Vector3Int.down * pos.y * 32);
+    } 
+        
+
+    /// <summary>
+    /// プレイヤーの次の目的地（座標）を取得する
+    /// ** 実際には移動はしない。移動確認用に使用する。
+    /// </summary>
+    /// <param name="playerDirection">移動方向</param>
+    /// <returns>移動予定の場所</returns>
+    public Vector3Int GetNextPosition(PlayerDirection playerDirection)
+    {
+        return (PlayerPos + _playerPosAddLists[playerDirection]);
+    }
+    
+    /// <summary>
+    /// 移動のコルーチン
+    /// </summary>
+    /// <param name="playerDirection">移動方向</param>
+    /// <returns></returns>
+    private IEnumerator Walking(PlayerDirection playerDirection)
+
+    {
+        //  キャラクター座標の更新
+        PlayerPos += _playerPosAddLists[playerDirection];
+        //  移動前の座標
+        Vector3 orgPos = transform.localPosition;
+        _ofp = Vector3.zero;
+        while (true)
+        {
+            //  加算する座標を計算する
+            _ofp                    += _playerWalkAddLists[playerDirection] * _walkSpeed;
+            //  移動範囲が１ブロックを超えたら、１ブロック分に直す
+            if (_ofp.magnitude > 32f)
+            {
+                _ofp                    = _playerWalkAddLists[playerDirection] * 32;
+                //  座標を更新する
+                transform.localPosition = orgPos + _ofp;
+                //  ループを抜ける
+                break;
+            }
+            //  座標を更新する
+            transform.localPosition =  orgPos + _ofp;
+            //  次の update まで待つ
+            yield return null;
+        }
+        _isWalking = false;
+        if(null != _walkEndCallback)
+            _walkEndCallback();
+    }
+}
